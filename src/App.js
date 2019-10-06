aba1d9d1ac4d0cd993a7bfe1ba986b8dfab00a57
Alex Borsbach <alexborsbach@Alexs-MacBook-Pro.local>
2019-10-06 18:54:36 -0500

    upload of router practice api inop

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Nav from './Nav'
+import About from './About'
+import Shop from './Shop'
+import ItemDetail from './ItemDetail'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route path='/about' component={About} />
+        <Route path='/shop' exact component={Shop} />
+        <Route path="/shop/:id" component={ItemDetail} />
+      </Switch>
     </div>
+    </BrowserRouter>
   );
 }
+
+const Home = () => (
+  <div>
+    <h1>Home Page</h1>
+  </div>
+)
 
 export default App;
